794089f0e3fe94a8dd3e2d5ec21d437777b8b11f
nrhint <nathan@hintonclan.org>
2019-05-20 10:06:19 -0600

    working on setting up.

---
+++ Main.py
@@ -0,0 +1,23 @@
+##Nathan Hinton
+##This program is for taking attendance for the High school.
+
+"""
+#Issure with current system:
+1) Teachers have to check into mutiple systems to see if a student was excused.
+"""
+
+import pickle
+import tkinter
+
+#Here are the vars that you can change:
+
+
+
+##This should be the main program
+
+#Load the database:
+
+pickle.load(database
+
+#Read the database:
+
